05a3066e460c3633ab3500c8ff7cf3e177adaec2
sweta chavan <124697568+swetaC1711@users.noreply.github.com>
2023-06-01 12:29:59 +0530

    Update writfile.c

--- writfile.c
+++ writfile.c
@@ -6,3 +6,23 @@ int main(){
     fprintf(fptr,"hello world");
     fclose(fptr);
 }
+
+//======================to take input from user(dynamic)============================
+    
+/* 
+#include<stdio.h>
+int main(){
+    FILE *fptr;
+    char c='a';
+    fptr=fopen("cust.txt","w");
+    if(fptr==NULL){
+        printf("file not found");
+    }
+    while(c!='@'){
+        scanf("%c",&c);
+        fputc(c,fptr);
+    }
+    fclose(fptr);
+    return 0;
+}*/
+
