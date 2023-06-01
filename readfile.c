634c664cc140a305b990508c52d0ec0fb0f65c7b
sweta chavan <124697568+swetaC1711@users.noreply.github.com>
2023-06-01 12:24:30 +0530

    Update readfile.c

--- readfile.c
+++ readfile.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+void
+main ()
+{
+  FILE *fp;
+  char ch;
+  fp = fopen ("cust.txt", "r");
+  while (1)
+    {
+      ch = fgetc (fp);
+      if (ch == EOF)
+	break;
+      printf ("%c", ch);
+    }
+  fclose (fp);
+}
 
